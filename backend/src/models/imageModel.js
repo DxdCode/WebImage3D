932c672fcb7133257f9b2c37c44a57b031795f5a
DxdCode <davidmuela2407@gmail.com>
2025-09-24 23:31:52 -0500

    feat(model): update image model to include guest user block and add meshy taskId

--- backend/src/models/imageModel.js
+++ backend/src/models/imageModel.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
 
 const imageSchema = new mongoose.Schema({
-  url: { type: String, required: true },            
-  public_id: { type: String, required: true },     
-  url_model: { type: String },                     
-  public_model_id: { type: String },                 
-});
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  public_id: { type: String, required: true },
+
+  url_model: { type: String },
+  public_model_id: { type: String },
+  task_id: { type: String },
+
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+  meta: {
+    ip: { type: String },
+    userAgent: { type: String },
+  }
+}, { timestamps: true });
 
 const Image = mongoose.model("Image", imageSchema);
 export default Image;
