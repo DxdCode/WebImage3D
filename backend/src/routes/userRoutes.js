b78f2a91ebe60f31f8d385bac66ad38c7eb377d2
DxdCode <davidmuela2407@gmail.com>
2025-09-25 23:46:02 -0500

    feat(auth): create login route for users and fix package.json

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createUser, getUsers, getUser, deleteUser } from "../controllers/userController.js";
+import { createUser, getUsers, getUser, deleteUser, loginUser } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Registro público
-router.post("/", createUser);
+router.post("/register", createUser);
+router.post("/login", loginUser);
+
 
 // Rutas protegidas
 router.get("/", authMiddleware, getUsers);
