32b03584c50fa9dc4333959999499f8d6fc0974e
DxdCode <davidmuela2407@gmail.com>
2025-09-29 23:02:32 -0500

    chore(project): clean structure, remove unnecessary files, update package.json

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,22 +1,9 @@
-import Header from './components/Header';
-import UploadSection from './components/UploadSection';
-import ImageList from './components/ImageList';
-import DetailSection from './components/DetailSection';
-import { APIProvider } from './context/APIContext';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <APIProvider>
-      <div className="bg-gray-100 min-h-screen">
-        <Header />
-        <div className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow">
-          <UploadSection />
-          <ImageList />
-          <DetailSection />
-        </div>
-      </div>
-    </APIProvider>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- fronted/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="bg-blue-800 text-white p-4 text-center text-xl">
-      API Imágenes & Modelos 3D
-    </header>
-  );
-};
-
-export default Header;
