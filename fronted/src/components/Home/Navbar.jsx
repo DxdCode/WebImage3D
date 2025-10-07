35818b5b4cab89232368919041d2c79f76e5c448
DxdCode <davidmuela2407@gmail.com>
2025-10-06 23:20:06 -0500

    "feat(navbar): add button to navigate to model dashboard"

--- fronted/src/components/Home/Navbar.jsx
+++ fronted/src/components/Home/Navbar.jsx
@@ -3,7 +3,9 @@ import { Box } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row items-center justify-between px-4 md:px-28 py-4 bg-green-900 dark:bg-neutral-900 relative">
+    <nav className="fixed top-0 left-0 w-full z-50 flex flex-row items-center justify-between px-4 md:px-28 py-4 bg-green-900 dark:bg-neutral-900 shadow-md">
+
+
       <Link className="flex items-center text-2xl md:text-3xl mb-2 md:mb-0" to="/">
         <Box size={28} className="mr-1 text-white" />
         <span className="text-white font-semibold">Dxd</span>
@@ -12,8 +14,14 @@ const Navbar = () => {
 
       <div className="flex flex-row items-center gap-2 md:gap-4">
         <Link
+          to="/models"
+          className="px-4 py-2 bg-neutral-800 text-white dark:bg-neutral-700 hover:bg-neutral-800 hover:text-white  rounded-md font-semibold transition-colors"
+        >
+          Modelos
+        </Link>
+        <Link
           to="/login"
-          className="px-4 py-2 bg-neutral-900 dark:bg-neutral-700 hover:bg-neutral-800 rounded-md font-semibold transition-colors"
+          className="px-4 py-2 bg-neutral-800 text-white dark:bg-neutral-700 hover:bg-neutral-800 hover:text-white  rounded-md font-semibold transition-colors"
         >
           Login
         </Link>
